adc062e192d545e22d74e8bf515ea1967a4526a1
Rahul462000 <Rahul2018ME015abesit@gmail.com>
2023-11-16 12:57:24 +0530

    Node with express basics

--- DeployWtihNode/index.js
+++ DeployWtihNode/index.js
@@ -1,1 +1,18 @@
-console.log("cahi aur code");
+// dot env is a production ready package
+require("dotenv").config;
+import express from "express";
+const app = express();
+const portNumner = 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello evryone");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>chai aur code</h2>");
+});
+
+// calling here the env with port number decalre on .env file
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${portNumner}`);
+});
